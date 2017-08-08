7860c90b882dbb1171415c31040032b2ab135349
Yannick <steveninyannick@gmail.com>
2017-08-08 19:00:18 -0300

    Layout and css for log in screen with material-ui

--- src/components/LogInScreen.jsx
+++ src/components/LogInScreen.jsx
@@ -1,6 +1,57 @@
-import React from 'react'
+import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import {Card} from 'material-ui';
 
+class LogInScreen extends Component {
 
-const LogInScreen = () => <h1>LogInScreen</h1>
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:''
+  }
+ }
+
+render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <Card className="container">
+               <form action="/">
+                 <h2 className="card-heading">Login</h2>
+
+                 <div className="field-line">
+                   <TextField
+                     floatingLabelText="Username"
+                     name="email"
+                   />
+                 </div>
+
+                 <div className="field-line">
+                   <TextField
+                     floatingLabelText="Password"
+                     type="password"
+                     name="password"
+                   />
+                 </div>
+
+                 <div className="button-line">
+                   <RaisedButton type="submit" label="Log in" primary />
+                 </div>
+               </form>
+             </Card>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
 
 export default LogInScreen;
