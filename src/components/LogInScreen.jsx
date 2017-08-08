92deaa182583e2bdf62c5fa3f81d0fff3d4e0aca
Yannick <steveninyannick@gmail.com>
2017-08-08 19:03:24 -0300

    Layout

--- src/components/LogInScreen.jsx
+++ src/components/LogInScreen.jsx
@@ -1,54 +1,40 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import {Card} from 'material-ui';
 
 class LogInScreen extends Component {
 
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
   }
- }
 
-render() {
+  render() {
     return (
       <div>
         <MuiThemeProvider>
           <div>
-          <AppBar
-             title="Login"
-           />
-           <Card className="container">
-               <form action="/">
-                 <h2 className="card-heading">Login</h2>
+            <Card className="container">
+              <h2 className="card-heading">Login</h2>
+              <div className="field-line">
+                <TextField floatingLabelText="Username" name="username"/>
+              </div>
 
-                 <div className="field-line">
-                   <TextField
-                     floatingLabelText="Username"
-                     name="email"
-                   />
-                 </div>
+              <div className="field-line">
+                <TextField floatingLabelText="Password" type="password" name="password"/>
+              </div>
 
-                 <div className="field-line">
-                   <TextField
-                     floatingLabelText="Password"
-                     type="password"
-                     name="password"
-                   />
-                 </div>
-
-                 <div className="button-line">
-                   <RaisedButton type="submit" label="Log in" primary />
-                 </div>
-               </form>
-             </Card>
-         </div>
-         </MuiThemeProvider>
+              <div className="button-line">
+                <RaisedButton label="Log in" primary/>
+              </div>
+            </Card>
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
