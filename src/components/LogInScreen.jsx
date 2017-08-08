c26c4ec432e94fc0bde69b231d9d68aabb912f4b
Yannick <steveninyannick@gmail.com>
2017-08-08 20:24:59 -0300

    Added login post and state structure to the loginScreen

--- src/components/LogInScreen.jsx
+++ src/components/LogInScreen.jsx
@@ -7,12 +7,24 @@ import {Card} from 'material-ui';
 class LogInScreen extends Component {
 
 
+  handleLogin = () =>{
+      let loginScreen = this
+      fetch('http://localhost:8080/api/login', {headers: new Headers({
+                  'Content-Type': 'application/json'
+              }), method: 'POST', body: JSON.stringify({"username":"pilar","password":"architect123"}) })
+              .then(function(res) {
+                  return res.json();
+              }).then(function(json) {
+                loginScreen.setState({'active_user' : json})
+              });
+  }
+
   onChangeUsername = event =>{
     this.setState({'username': event.target.value })
   }
 
   onChangePassword = event =>{
-    this.setState({'username': event.target.value })
+    this.setState({'password': event.target.value })
   }
 
 
@@ -21,11 +33,13 @@ class LogInScreen extends Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      active_user : {}
     }
 
     this.onChangePassword = this.onChangePassword.bind(this)
     this.onChangeUsername = this.onChangeUsername.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
   }
 
 
@@ -45,7 +59,7 @@ class LogInScreen extends Component {
               </div>
 
               <div className="button-line">
-                <RaisedButton label="Log in" primary/>
+                <RaisedButton label="Log in" primary onClick={this.handleLogin}/>
               </div>
             </Card>
           </div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+injectTapEventPlugin();
 
 ReactDOM.render((
   <BrowserRouter>
